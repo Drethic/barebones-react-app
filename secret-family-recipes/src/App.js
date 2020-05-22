4d66f6b2aec5b15eba5a52e4c4be673da01517a5
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-05-22 00:04:51 -0400

    Add Home route, redirects for netlify

--- secret-family-recipes/src/App.js
+++ secret-family-recipes/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import {Route, link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Login from './components/LoginPage';
 import './App.css';
 
+// import PrivateRoute from './components/PriviteRoute';
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className='App'>
+        <div className='nav'>
+          <Link to='/login'>Login</Link>
+        </div>
+        <div className='routes'>
+          <Switch>
+            <Route path='/login' component={Login} />
+            <Route exact path='/' component={Login} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
