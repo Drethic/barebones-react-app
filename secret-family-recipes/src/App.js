66ea2a1ac6474ebb8296871589c83fac5a29238d
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-05-22 19:46:02 -0400

    Troubleshooting Deploy

--- secret-family-recipes/src/App.js
+++ secret-family-recipes/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import Login from './components/LoginPage';
 import './App.css';
 import PrivateRoute from './components/PriviteRoute';
@@ -8,20 +8,18 @@ import PrivateRoute from './components/PriviteRoute';
 import Header from './components/Header';
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <div className='nav-container'>
-          <Header />
-        </div>
-        <div className='routes'>
-          <Switch>
-            <Route path='/login' component={Login} />
-            <Route exact path='/' component={Login} />
-            {/* <PrivateRoute path='/recipe-home' component={} /> */}
-          </Switch>
-        </div>
+    <div className='App'>
+      <div className='nav-container'>
+        <Header />
       </div>
-    </Router>
+      <div className='routes'>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route exact path='/' component={Login} />
+          {/* <PrivateRoute path='/recipe-home' component={} /> */}
+        </Switch>
+      </div>
+    </div>
   );
 }
 
