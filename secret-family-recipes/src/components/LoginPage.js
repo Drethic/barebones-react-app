4093134a215db2eb0ab36b3189cac3ef921c4599
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-05-22 21:39:32 -0400

    trouble shoot deploy--- new log in page added

--- secret-family-recipes/src/components/LoginPage.js
+++ secret-family-recipes/src/components/LoginPage.js
@@ -1,107 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import { useForm } from 'react-hook-form';
-import * as yup from 'yup';
+import React, { useState } from 'react';
 
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     '& .MuiTextField-root': {
-//       margin: theme.spacing(1),
-//       width: 200,
-//     },
-//   },
-// }));
+import AxiosWithAuth from '../utils/AxiosWithAuth';
 
-//Login Validation
-const loginSchema = yup.object().shape({
-  email: yup
-    .string()
-    .email('Must be a Valid email address.')
-    .required('You must include an email address.'),
-  password: yup.string().required('Please enter your password.'),
-});
-
-//Registration Validation
-const regiterSchema = yup.object().shape({
-  name: yup.string(),
-  email: yup
-    .string()
-    .email('Must be a Valid email address.')
-    .required('You must include an email address.'),
-  password: yup.string().required('Please enter a password.'),
-});
-
-function Login() {
-  // const classes = useStyles();
-  // const { register, handleSubmit, errors } = useForm();
-  // const onSubmit = data => {
-  //   console.log(data);
-  // };
-  // your form submit function which will invoke after successful validation
-  // Login default state
-  const [loginState, setLoginState] = useState({
-    email: '',
+function Login(props) {
+  const [creds, setCreds] = useState({
+    username: '',
     password: '',
   });
-
-  //Register default State
-  const [registerState, setRegisterState] = useState({
-    name: '',
-    email: '',
-    password: '',
-  });
-  //Button disabled until all fields meet schema
-  const [loginButtonDisabled, setLoginButtonDisabled] = useState(true);
-
-  useEffect(() => {
-    loginSchema.isValid(loginState).then(valid => {
-      setLoginButtonDisabled(!valid);
+  const handleChange = e => {
+    setCreds({
+      ...creds,
+      [e.target.name]: e.target.value,
     });
-  }, [loginState]);
-
+  };
+  const login = e => {
+    e.preventDefault();
+    console.log(creds);
+    AxiosWithAuth()
+      .post('/login', creds)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/friendslist');
+      })
+      .catch(err => {
+        console.log('Err is: ', err);
+      });
+  };
   return (
-    //container for login element
-    <div className='loginContainer'>
-      <form
-        // className={classes.root}
-        noValidate
-        autoComplete='off'
-        // onSubmit={handleSubmit(onSubmit)}
-      >
-        {/* <div className='LoginFieldEmail'> */}
-        {/* email/UN text field */}
-        <TextField
-          // error={errors.email}
-          id='email'
-          label='email'
-          variant='outlined'
-          // helperText={errors.email}
-          // ref={register({ required: true, maxLength: 10 })}
+    <section className='loginForm'>
+      <form onSubmit={login}>
+        <label>User Name:</label>
+        <input
+          type='text'
+          name='username'
+          value={creds.username}
+          onChange={handleChange}
         />
-        {/* {errors.email} */}
-        {/* <div className='LoginFieldEmail'> */}
-        {/* Password text field */}
-        <TextField id='outlined-basic' label='password' variant='outlined' />
-        {/* </div> */}
-        {/* </div> */}
-        {/* <div className='LoginButtons'> */}
-        {/* Login Button */}
-        <Button variant='contained' color='primary'>
-          Login
-        </Button>
-        {/* Registration Button */}
-        <Button variant='contained' color='primary'>
-          Register
-        </Button>
-        {/* </div> */}
+        <br />
+        <label>Password: </label>
+        <input
+          type='password'
+          name='password'
+          value={creds.password}
+          onChange={handleChange}
+        />
+        <br />
+        <button type='submit'>Log In</button>
       </form>
-    </div>
+    </section>
   );
 }
-
 export default Login;
