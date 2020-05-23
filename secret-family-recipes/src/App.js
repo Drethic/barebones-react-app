dc126eb808da2a212764cb270ed1c8306b100238
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-05-22 20:24:07 -0400

    Troubleshooting Deploy, Moved Browser Router

--- secret-family-recipes/src/App.js
+++ secret-family-recipes/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Login from './components/LoginPage';
 import './App.css';
 import PrivateRoute from './components/PriviteRoute';
@@ -8,18 +8,20 @@ import PrivateRoute from './components/PriviteRoute';
 import Header from './components/Header';
 function App() {
   return (
-    <div className='App'>
-      <div className='nav-container'>
-        <Header />
+    <Router>
+      <div className='App'>
+        <div className='nav-container'>
+          <Header />
+        </div>
+        <div className='routes'>
+          <Switch>
+            <Route path='/login' component={Login} />
+            <Route exact path='/' component={Login} />
+            {/* <PrivateRoute path='/recipe-home' component={} /> */}
+          </Switch>
+        </div>
       </div>
-      <div className='routes'>
-        <Switch>
-          <Route path='/login' component={Login} />
-          <Route exact path='/' component={Login} />
-          {/* <PrivateRoute path='/recipe-home' component={} /> */}
-        </Switch>
-      </div>
-    </div>
+    </Router>
   );
 }
 
