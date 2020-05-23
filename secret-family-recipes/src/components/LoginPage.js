88e33fedd6121962eef66bd54a249fc5e114537e
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-05-22 21:08:11 -0400

    Troubleshoot deploy== comment out all form elements

--- secret-family-recipes/src/components/LoginPage.js
+++ secret-family-recipes/src/components/LoginPage.js
@@ -65,46 +65,47 @@ function Login() {
   }, [loginState]);
 
   return (
-    //container for login element
-    <div className='loginContainer'>
-      <form
-        className={classes.root}
-        noValidate
-        autoComplete='off'
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className='LoginFieldEmail'>
-          {/* email/UN text field */}
-          <TextField
-            error={errors.email}
-            id='email'
-            label='email'
-            variant='outlined'
-            helperText={errors.email}
-            ref={register({ required: true, maxLength: 10 })}
-          />
-          {errors.email}
-          <div className='LoginFieldEmail'>
-            {/* Password text field */}
-            <TextField
-              id='outlined-basic'
-              label='password'
-              variant='outlined'
-            />
-          </div>
-        </div>
-        <div className='LoginButtons'>
-          {/* Login Button */}
-          <Button variant='contained' color='primary'>
-            Login
-          </Button>
-          {/* Registration Button */}
-          <Button variant='contained' color='primary'>
-            Register
-          </Button>
-        </div>
-      </form>
-    </div>
+    <div>FIXED!</div>
+    // //container for login element
+    // <div className='loginContainer'>
+    //   <form
+    //     className={classes.root}
+    //     noValidate
+    //     autoComplete='off'
+    //     onSubmit={handleSubmit(onSubmit)}
+    //   >
+    //     <div className='LoginFieldEmail'>
+    //       {/* email/UN text field */}
+    //       <TextField
+    //         error={errors.email}
+    //         id='email'
+    //         label='email'
+    //         variant='outlined'
+    //         helperText={errors.email}
+    //         ref={register({ required: true, maxLength: 10 })}
+    //       />
+    //       {errors.email}
+    //       <div className='LoginFieldEmail'>
+    //         {/* Password text field */}
+    //         <TextField
+    //           id='outlined-basic'
+    //           label='password'
+    //           variant='outlined'
+    //         />
+    //       </div>
+    //     </div>
+    //     <div className='LoginButtons'>
+    //       {/* Login Button */}
+    //       <Button variant='contained' color='primary'>
+    //         Login
+    //       </Button>
+    //       {/* Registration Button */}
+    //       <Button variant='contained' color='primary'>
+    //         Register
+    //       </Button>
+    //     </div>
+    //   </form>
+    // </div>
   );
 }
 
