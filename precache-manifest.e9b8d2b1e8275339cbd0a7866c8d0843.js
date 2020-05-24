20ef02600e7f7b5f0bdbe641dfdb1db3dd38f49e
Drethic <drethic@msn.com>
2020-05-24 18:34:04 -0400

    Updates

---
+++ precache-manifest.e9b8d2b1e8275339cbd0a7866c8d0843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58870f0a7c8602396c0c778da4546383",
+    "url": "/barebones-react-app/index.html"
+  },
+  {
+    "revision": "d651eb468e812fb6a217",
+    "url": "/barebones-react-app/static/css/main.b3492f43.chunk.css"
+  },
+  {
+    "revision": "ed13b6ef16892ad5c475",
+    "url": "/barebones-react-app/static/js/2.67ede13e.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/barebones-react-app/static/js/2.67ede13e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d651eb468e812fb6a217",
+    "url": "/barebones-react-app/static/js/main.bf6c831f.chunk.js"
+  },
+  {
+    "revision": "253a15f14e6da224188b",
+    "url": "/barebones-react-app/static/js/runtime-main.644cbfcc.js"
+  }
+]);
