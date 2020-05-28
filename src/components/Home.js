c3f44dca5587e8b53f39d5105e5095aa5a33702f
Drethic <drethic@msn.com>
2020-05-27 23:39:51 -0400

    Fake Form * Organized code in Home.js * Added comments to make it easier to move to correct components

--- src/components/Home.js
+++ src/components/Home.js
@@ -111,54 +111,17 @@ function Home(props) {
   }, []);
 
   // Start of making a fake recipe
+
+  // stubData
+  const fakeExistingIngredients = ingredients.slice(2, 4);
   const fakeRecipe = {
     user_id: user.id,
     category_id: 1,
     title: 'Test Title',
     description: 'Test Description',
   };
-  const addRecipeToState = () => {
-    const updatedRecipe = {
-      ...newRecipe,
-      recipe: fakeRecipe,
-    };
-    console.log({ updatedRecipe });
-    setNewRecipe(updatedRecipe);
-  };
 
-  const fakeExistingIngredients = ingredients.slice(2, 4);
-
-  const instructionsToAdd = [];
-  // TODO pass in instructions to push into state
-  const addInstructionsToState = () => {
-    for (let i = 1; i < 5; i += 1) {
-      instructionsToAdd.push({
-        step_no: `${i}`,
-        instruction: `Step ${i}`,
-      });
-    }
-    setNewRecipe({
-      ...newRecipe,
-      instructions: [
-        ...newRecipe.instructions,
-        ...instructionsToAdd,
-      ],
-    });
-  };
-
-  const postNewIngredients = async (mapIngredients) => (
-    Promise.all(mapIngredients.map((ingredient) => (
-      AxiosWithAuth()
-        .post('/ingredients', ingredient)
-        .then((res) => (
-          {
-            ...ingredient,
-            ...res.data,
-          }
-        ))
-    )))
-  );
-
+  // Step 2 API
   const addIngredientToRecipe = (recipeId, ingredient) => {
     // {id: 4, name: "Garlic", quantity: 1.5, unit_id: 10}
     const recipeIng = {
@@ -175,7 +138,6 @@ function Home(props) {
       })
       .catch((err) => console.log('User Get Err:', err));
   };
-
   const addIngredientsToRecipe = async (recipeId) => {
     if (newRecipe.ingredients.length > 0) {
       Promise.all(newRecipe.ingredients.map((ingredient) => (
@@ -184,6 +146,7 @@ function Home(props) {
     }
   };
 
+  // Step 3 API
   const addInstructionToRecipe = (recipeId, instruction) => {
     const recipeInst = {
       recipe_id: recipeId,
@@ -198,7 +161,6 @@ function Home(props) {
       })
       .catch((err) => console.log('User Get Err:', err));
   };
-
   const addInstructionsToRecipe = async (recipeId) => {
     if (newRecipe.instructions.length > 0) {
       Promise.all(newRecipe.instructions.map((instruction) => (
@@ -207,14 +169,47 @@ function Home(props) {
     }
   };
 
-  const setRecipeInState = () => {
-    console.log('Adding fake recipe to state');
-    addRecipeToState();
+  // Final Step API
+  const addFakeRecipe = async () => {
+    // Submit recipe
+    // Step 1 API
+    const recipeData = await AxiosWithAuth().post('/recipes', fakeRecipe).then((res) => res.data);
+    // Add ingredients to recipe
+    await addIngredientsToRecipe(recipeData.recipe_id);
+    // Add instructions to recipe
+    await addInstructionsToRecipe(recipeData.recipe_id);
   };
 
-  const setIngredientsToState = async () => {
+  // MOVE ME TO AddRecipe component!!!
+  // Step 1
+  // TODO Add recipe from form values
+  const setRecipeInState = (/* Pass recipe form values here!!! */) => {
+    console.log('Adding fake recipe to state');
+    setNewRecipe({
+      ...newRecipe,
+      recipe: fakeRecipe,
+    });
+  };
+
+  // Step 2
+  // Helper method for setIngredientsToState
+  const postNewIngredients = async (mapIngredients) => (
+    Promise.all(mapIngredients.map((ingredient) => (
+      AxiosWithAuth()
+        .post('/ingredients', ingredient)
+        .then((res) => (
+          {
+            ...ingredient,
+            ...res.data,
+          }
+        ))
+    )))
+  );
+  const setIngredientsToState = async (/* Pass ingredients form values here!!! */) => {
     // Adds new ingredients (if any) to DB
     console.log('Adding new ingredients to DB');
+    // TODO Replace with values from form
+    // TODO Need to check all incoming values to see if they need to be added
     const ingredientsToAdd = [
       {
         name: 'Paprika2',
@@ -227,10 +222,12 @@ function Home(props) {
         quantity: 1.5,
       },
     ];
+    // Wrap this with a check if there are ingredients to add
     const newIng = await postNewIngredients(ingredientsToAdd);
     console.log({ setIngNewIng: newIng });
 
     console.log('Adding fake ingredients to state');
+    // Replace stub info with actual existing ingredients
     const randomNum = Math.abs(Math.floor(Math.random() * (1 - 12) + 1));
     const updatedIngredients = fakeExistingIngredients.map((ingredient) => ({
       ...ingredient,
@@ -247,18 +244,24 @@ function Home(props) {
     });
   };
 
-  const setInstructionsToState = () => {
-    console.log('Adding instructions to state');
-    addInstructionsToState();
-  };
-
-  const addFakeRecipe = async () => {
-    // Submit recipe
-    const recipeData = await AxiosWithAuth().post('/recipes', fakeRecipe).then((res) => res.data);
-    // Add ingredients to recipe
-    await addIngredientsToRecipe(recipeData.recipe_id);
-    // Add instructions to recipe
-    await addInstructionsToRecipe(recipeData.recipe_id);
+  // Step 3
+  const instructionsToAdd = [];
+  // TODO pass in instructions to push into state
+  const setInstructionsToState = (/* Pass instruction form values here!!! */) => {
+    for (let i = 1; i < 5; i += 1) {
+      instructionsToAdd.push({
+        step_no: `${i}`,
+        instruction: `Step ${i}`,
+      });
+    }
+    setNewRecipe({
+      ...newRecipe,
+      instructions: [
+        ...newRecipe.instructions,
+        // TODO replace with param
+        ...instructionsToAdd,
+      ],
+    });
   };
 
   return (
@@ -278,6 +281,8 @@ function Home(props) {
       </form>
       <div>
         <CardContent recipes={recipes} />
+        {/* <AddRecipe newRecipe={newRecipe} setNewRecipe={setNewRecipe} /> */}
+        {/* Move all 3 buttons associated logic to the AddRecipe container */}
         <button type="button" onClick={setRecipeInState}>Step 1 Next</button>
         <button type="button" onClick={setIngredientsToState}>Step 2 Next</button>
         <button type="button" onClick={setInstructionsToState}>Step 3 Next</button>
